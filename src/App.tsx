1b3307238d2bc2378d1faa1156fac0f73d27f8cd
Ana <arperezch13@alumnos.cei.es>
2026-03-03 20:58:40 +0100

    Fix component import and correct PlanCard prop name in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import StockBadge from './components/StockBadge/StockBadge'
 import PlanCard from './components/PlanCard/PlanCard'
+import Notifications from './components/Notifications/Notifications'
 
 function App() {
 
@@ -11,12 +12,12 @@ function App() {
       <StockBadge productName="Gorra" stock={5} />
       <StockBadge productName="Taza" stock={15} />
 
-      <PlanCard planName='Bassic' price={5} isPopular/>
-      <PlanCard planName='Pro' price={15} isPopular={false}/>
+      <PlanCard name='Bassic' price={5} isPopular/>
+      <PlanCard name='Pro' price={15} isPopular={false}/>
 
-      <Notification message= 'Success operation completed' type='success'/>
-      <Notification message= 'Warning: Check your input' type='alert'/>
-      <Notification message= 'Error: Something went wrong' type='error'/>
+      <Notifications message= 'Success operation completed' type='success'/>
+      <Notifications message= 'Warning: Check your input' type='alert'/>
+      <Notifications message= 'Error: Something went wrong' type='error'/>
       
     </>
   )
