aafd929a9f50d69791d058501f48a9900263ec45
Ana <arperezch13@alumnos.cei.es>
2026-03-03 20:44:49 +0100

    Remove Server component imports and instances from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css'
-import Server from './components/Server/Server'
 import StockBadge from './components/StockBadge/StockBadge'
 import PlanCard from './components/PlanCard/PlanCard'
-import Notification from './components/Notification/Notification'
 
 function App() {
 
   return (
     <>
-      <Server serverName="Web principal" status="online" />
-      <Server serverName="Base de datos" status="online" />
-      <Server serverName="Api de pagos" status="offline" />
 
       <StockBadge productName="Camiseta" stock={0} />
       <StockBadge productName="Gorra" stock={5} />
