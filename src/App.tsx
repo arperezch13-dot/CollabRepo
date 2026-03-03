92aca603d23535d89ce67c5ade5a9bdb4ed881d6
Ana <arperezch13@alumnos.cei.es>
2026-03-03 21:52:27 +0100

    Refactor App component to use Counter and add CardPreview component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import './App.css'
-import StockBadge from './components/StockBadge/StockBadge'
-import PlanCard from './components/PlanCard/PlanCard'
-import Notifications from './components/Notifications/Notifications'
+import Counter from './components/Counter/Counter'
 
 function App() {
 
   return (
     <>
 
-      <StockBadge productName="Camiseta" stock={0} />
-      <StockBadge productName="Gorra" stock={5} />
-      <StockBadge productName="Taza" stock={15} />
-
-      <PlanCard name='Bassic' price={5} isPopular/>
-      <PlanCard name='Pro' price={15} isPopular={false}/>
-
-      <Notifications message= 'Success operation completed' type='success'/>
-      <Notifications message= 'Warning: Check your input' type='alert'/>
-      <Notifications message= 'Error: Something went wrong' type='error'/>
+     <> {<Counter />}</>
       
     </>
   )
