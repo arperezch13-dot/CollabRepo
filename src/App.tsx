da97ded2fba4ca90898739f69390703829d55a5e
Ana <arperezch13@alumnos.cei.es>
2026-03-05 21:58:55 +0100

    Remove unused imports of Counter and Interruptor components from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css'
-import Counter from './components/Counter/Counter'
-import Interruptor from './components/Interruptor/Interruptor'
+
+
 function App() {
 
   return (
     <>
-
-     <> {<Counter />}</>
-      <> {<Interruptor />}</>
-      
+  
     </>
   )
 }
